45e31bc805388d7035f380bc79bb6d2fae2435d3
Joel Mavinga-Fernandes <joelfernandes35@hotmail.com>
2022-08-11 23:04:10 -0300

    conultas adicionadas

--- src/main/java/br/com/store/dao/ProdutoDao.java
+++ src/main/java/br/com/store/dao/ProdutoDao.java
@@ -1,5 +1,8 @@
 package br.com.store.dao;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
 import br.com.store.model.Categoria;
@@ -16,5 +19,37 @@ public class ProdutoDao {
 	public void cadastrar (Produto produto) {
 		this.em.persist(produto);
 	}
+	
+	public Produto buscarPorId(Long id) {
+		return this.em.find(Produto.class, id);
+	}
+	
+	public List<Produto> findAll(){
+		String jpql = "SELECT p FROM Produto p";
+		return em.createQuery(jpql, Produto.class).getResultList();
+	}
+	
+	public List<Produto> findByName(String nome){
+		String jpql = "SELECT p FROM Produto p WHERE p.nome= :nome ";
+		return em.createQuery(jpql, Produto.class)
+		.setParameter("nome", nome)
+		.getResultList();
+	}
+	
+	public List<Produto> findByCategoriaName(String nome){
+		String jpql = "SELECT p FROM Produto p WHERE p.categoria.nome= :nome ";
+		return em.createQuery(jpql, Produto.class)
+		.setParameter("nome", nome)
+		.getResultList();
+	}
+	
+	public BigDecimal findPriceByProductName(String nome){
+		String jpql = "SELECT p.preco FROM Produto p WHERE p.nome= :nome ";
+		return em.createQuery(jpql, BigDecimal.class)
+		.setParameter("nome", nome)
+		.getSingleResult();
+	}
+	
+	
 
 }
--- src/main/java/br/com/store/testes/CadastroDeProdutos.java
+++ src/main/java/br/com/store/testes/CadastroDeProdutos.java
@@ -1,6 +1,7 @@
 package br.com.store.testes;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -16,28 +17,44 @@ public class CadastroDeProdutos {
 
 	public static void main(String[] args) {
 		
+		cadastrarProduto();
+		EntityManager em = JPAUtil.getEntityManager();
+		ProdutoDao produtoDao = new ProdutoDao(em);
+		
+		Produto p = produtoDao.buscarPorId(1l);
+		System.out.println(p.getPreco());
+		
+		 List<Produto> all = produtoDao.findAll();
+		 all.forEach(p1 -> System.out.println(p.getNome()));
+		 
+		 List<Produto> findByName = produtoDao.findByName("IPhone");
+		 findByName.forEach(p2 -> System.out.println(p.getNome()));
+		 
+		 List<Produto> findByCategoriaName = produtoDao.findByCategoriaName("CELULARES");
+		 findByCategoriaName.forEach(p3 -> System.out.println(p.getNome()));
+		 
+		 BigDecimal precoDoProduto = produtoDao.findPriceByProductName("IPhone");
+		 System.out.println("Preço do produto: " + precoDoProduto);
+		
+		
+
+		
+	}
+
+	private static void cadastrarProduto() {
 		Categoria celulares = new Categoria("CELULARES");
-		
+
 		Produto celular = new Produto("IPhone", "Pro Max", new BigDecimal(1220), celulares);
-		
+
 		EntityManager em = JPAUtil.getEntityManager();
 		ProdutoDao produtoDao = new ProdutoDao(em);
 		CategoriaDao categoriaDao = new CategoriaDao(em);
-		
+
 		em.getTransaction().begin();
 		categoriaDao.cadastrar(celulares);
 		produtoDao.cadastrar(celular);
 		em.flush();
 		em.clear();
-		
-		celulares = em.merge(celulares);
-		celulares.setNome("Iphone 13");
-		em.flush();
-		em.clear();
-		
-		em.remove(celulares);
-		em.flush();
-		
 	}
 
 }
